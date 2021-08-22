933af977159cc06d747178514473a7758043909f
Joshua Hall <43988692+FoobarQ@users.noreply.github.com>
2021-08-22 23:33:10 +0100

    (*) fixed: capitalisation with apostrophes, and text size problem
    (*) fixed: sentence placement to fit everything on screen

---
+++ utils.py
@@ -0,0 +1,6 @@
+import re
+
+
+def title_case(s: str) -> str:
+    # fix with regex stuff I guess
+    return re.sub(r"([0-9][A-Z][a-z])", r"\1".lower(), s.title()).replace("'S", "'s").replace("Ii", "II")
