1649200aa9c2cc8463c5342644ec80c37dba3686
Foobar Q <joshua.hall2020@outlook.com>
2021-09-01 23:05:12 +0100

    (*) fixed: formatting in general

--- utils.py
+++ utils.py
@@ -3,4 +3,9 @@ import re
 
 def title_case(s: str) -> str:
     # fix with regex stuff I guess
-    return re.sub(r"([0-9][A-Z][a-z])", r"\1".lower(), s.title()).replace("'S", "'s").replace("Ii", "II")
+    return (
+        re.sub(r"([0-9][A-Z][a-z])", r"\1".lower(), s.title())
+        .replace("'S", "'s")
+        .replace("Ii", "II")
+        .replace("'T", "'t")
+    )
